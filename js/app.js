00e58b9e92400a8392b6fc92d9e9f53be18adcb3
greg-colin <greg-colin@users.noreply.github.com>
2015-02-28 17:03:05 -0500

    A try at fixing the marker info window problem, which failed.

--- js/app.js
+++ js/app.js
@@ -5,55 +5,31 @@
 // View Model
 var MapViewModel = function() {
   var self = this;
-  var iconBase = "./images/google_map_markers/"
+  var iconBase = "./images/google_map_markers/";
   var userLat = ko.observable(40.7586);
   var userLong = ko.observable(-73.9792);
   var myLocation = new google.maps.LatLng(userLat(), userLong());
-  var foursquareVenues = ko.observableArray();
-  var mapMarkers = ko.observableArray();
-  var myLocationMarker = ko.observable();
+  var foursquareVenues = ko.observableArray([]);
+  this.mapMarkers = ko.observableArray([]);
+  this.myLocationMarker = ko.observable();
 
-  self.markOwnLocation = function() {
-    myLocationMarker(new google.maps.Marker({
-      map: map,
+  this.markOwnLocation = function() {
+    self.myLocationMarker(new google.maps.Marker({
+      map: self.map,
       position: myLocation,
       icon: iconBase + 'green_MarkerA.png',
       title: "My current location"
-    }))
-  }
-
-  self.createMapMarker = function(venue) {
-    var googleLatLng = new google.maps.LatLng(venue.location.lat, venue.location.lng);
-
-    var marker = new google.maps.Marker({
-      map: map,
-      position: googleLatLng,
-      icon: iconBase + 'purple_MarkerA.png',
-      title: venue.name
-    });
-
-    var infoWindow = new google.maps.InfoWindow({
-      content: "This is: " + venue.name
-    });
-
-    // hmmmm, I wonder what this is about...
-    google.maps.event.addListener(marker, 'click', function() {
-      console.log(infoWindow);
-      marker.setIcon(iconBase + 'red_MarkerA.png');
-      infoWindow.open(map, marker);
-    });
-
-    google.maps.event.addListener(infoWindow, 'closeclick', function() {
-      marker.setIcon(iconBase + 'purple_MarkerA.png');
-    });
-    mapMarkers().push(marker);
-  }
+    }));
+  };
 
   self.getVenues = function() {
     $.ajax({
         type: "GET",
-        url: "https://api.foursquare.com/v2/venues/search?ll="+userLat()+","+userLong()+"&client_id=OLYPOBMQ003QZVMZGDFOEGEZOGZQPNX1X404PVV1FLPVGFMU&client_secret=3MVWXXYQ5ENWZ4MKW4Q1NDMW2P20UFO243POFRBDZUHALQ4U&v=20150217",
+        url: "https://api.foursquare.com/v2/venues/search?limit=20&ll="+userLat()+","+userLong()+"&client_id=OLYPOBMQ003QZVMZGDFOEGEZOGZQPNX1X404PVV1FLPVGFMU&client_secret=3MVWXXYQ5ENWZ4MKW4Q1NDMW2P20UFO243POFRBDZUHALQ4U&v=20150228",
           success: function(data) {
+            var phone, category, address, rating;
+            console.log("response JSON:");
+            console.log(JSON.stringify(data));
             foursquareVenues(data.response.venues);
               $("#venues").html("");
               $.each(foursquareVenues(), function() {
@@ -80,15 +56,39 @@ var MapViewModel = function() {
                   } else {
                     rating = "";
                   }
-                  self.createMapMarker(this);
+                  console.log("creating marker");
+                  var appendeddatahtml = '<div class="venue"><h2><span>'+this.name+category+rating+'</span></h2>'+address+phone+'</p><p><strong>Total Checkins:</strong> '+this.stats.checkinsCount+'<p>Lat: '+this.location.lat+'<br>'+'Long: '+this.location.lng+'</p></div>';
+                  var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(this.location.lat, this.location.lng),
+                    title: this.name,
+                    icon: iconBase + 'purple_MarkerA.png',
+                    map: self.map
+                  });
                   //
-                  appendeddatahtml = '<div class="venue"><h2><span>'+this.name+category+rating+'</span></h2>'+address+phone+'</p><p><strong>Total Checkins:</strong> '+this.stats.checkinsCount+'</p></div>';
-                  $("#venues").append(appendeddatahtml);
-                  
+                  console.log(marker);
+                  //
+                  google.maps.event.addListener(marker, 'click', (function(marker) {
+                    return function() {
+                      console.log("Marker clicked:");
+                      console.log(marker);
+                      marker.setIcon(iconBase + 'red_MarkerA.png');
+                      self.handleInfoWindow(marker.position, appendeddatahtml);
+                    };
+                  })(marker));
+                  self.mapMarkers.push({marker: marker, content: appendeddatahtml});
               });
           }
     });
-  }
+  };
+
+  self.handleInfoWindow = function(latlng, content) {
+    console.log(latlng.toString());
+    self.infoWindow.setContent(content);
+    self.infoWindow.setPosition(latlng);
+    console.log("opening info window:");
+    console.log(self.infoWindow);
+    self.infoWindow.open(self.map);
+  };
 
   self.initialize = function() {
     console.log("in self.initialize");
@@ -97,14 +97,16 @@ var MapViewModel = function() {
       center: myLocation, // referencing the myLocation variable
       position: myLocation,  // same as above
       mapTypeId: google.maps.MapTypeId.ROADMAP,
-      zoom: 16
+      zoom: 17
     };
-    map = new google.maps.Map(document.getElementById('map-div'), mapOptions);
+    self.map = new google.maps.Map(document.getElementById('map-div'), mapOptions);
+    self.infoWindow = new google.maps.InfoWindow();
+    self.markOwnLocation();
+    self.getVenues();
   };
 
   self.initialize();
-  self.markOwnLocation();
-  self.getVenues();
+
 };
 
 var myMapViewModel = new MapViewModel();
